10ca464ed7b475f4b9f1a825e02d94dc9f222e79
Paras Bhatia <159020139+ParasBhatia999@users.noreply.github.com>
2024-09-07 17:41:33 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,50 @@
+#Task 2 Given by Codsoft
+# Python program to create a simple calculator
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero"
+    return x / y
+
+def calculator():
+    while True:
+        print("\nSelect operation:")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Multiply")
+        print("4. Divide")
+        print("5. Exit")
+
+        choice = input("Enter choice (1/2/3/4/5): ")
+
+        if choice == '5':
+            print("Exiting the calculator.")
+            break  # Exit the loop and end the program
+
+        if choice in ['1', '2', '3', '4']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print(f"{num1} + {num2} = {add(num1, num2)}")
+
+            elif choice == '2':
+                print(f"{num1} - {num2} = {subtract(num1, num2)}")
+
+            elif choice == '3':
+                print(f"{num1} * {num2} = {multiply(num1, num2)}")
+
+            elif choice == '4':
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
+        else:
+            print("Invalid Input. Please try again.")
+calculator()
